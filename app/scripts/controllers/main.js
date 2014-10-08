770cd54d8663056374fda0e6397c2522dbd9510a
Purvi Jain <purvi@snapick.me>
2014-10-08 21:16:29 +0530

    First version

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name txtWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the txtWebsiteApp
+ */
+angular.module('txtWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
